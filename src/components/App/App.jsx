287682de50476524291b4f629bd6b72f636a824c
Vovchick4 <vovaspodpivas@gmail.com>
2022-06-12 12:13:58 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,19 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import { Layout } from "../";
+
 import routes from "../../config/routes";
 
 export default function App() {
   return (
     <Suspense fallback={<span>Load</span>}>
       <Routes>
-        {routes.map(({ path, component: Component, ...rest }) => (
-          <Route key={path} path={path} element={<Component />} {...rest} />
-        ))}
+        <Route element={<Layout />}>
+          {routes.map(({ path, component: Component, ...rest }) => (
+            <Route key={path} path={path} element={<Component />} {...rest} />
+          ))}
+        </Route>
       </Routes>
     </Suspense>
   );
