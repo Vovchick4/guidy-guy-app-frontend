d379def3c8fe035c4b6b52906fb57b512176557e
Vovchick4 <vovaspodpivas@gmail.com>
2022-07-22 19:48:22 +0300

    fix

--- src/pages/Places/ui/AddPlace/AddPlace.jsx
+++ src/pages/Places/ui/AddPlace/AddPlace.jsx
@@ -1,5 +1,24 @@
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
 import styles from "./AddPlace.module.css";
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
+  coordinates: Yup.object({}).required("Required"),
+});
+
 export default function AddPlace() {
-  return <div>AddPlace</div>;
+  const formik = useFormik({
+    initialValues: { name: "", coordinates: {} },
+    validationSchema,
+    onSubmit: (values) => {},
+  });
+
+  return (
+    <div className={styles.content}>
+      <form></form>
+      <div></div>
+    </div>
+  );
 }
